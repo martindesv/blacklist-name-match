168e2eec5621a49dd812f44be166c894030fbdbd
Martin A <martin.apalkov@gmail.com>
2022-08-13 12:46:52 +0300

    hashset

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,28 +2,41 @@ package src.main.java;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start program");
+
+        String[] inputNameArray;
+        inputNameArray = args[0].split(" ");
+
+        HashSet<String> inputNameSet = new HashSet<String>(Arrays.asList(inputNameArray));
+
         try (Scanner scanner = new Scanner(new File(args[1]))) {
             while (scanner.hasNext()) {
-                checkNames(scanner.nextLine(), args[0]);
+                String[] listNameArray;
+                listNameArray = scanner.nextLine().split(" ");
+                HashSet<String> listNameSet = new HashSet<String>(Arrays.asList(listNameArray));
+                checkNames(inputNameSet, listNameSet);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static void checkNames(String line, String name) {
-        Pattern pattern = Pattern.compile(name, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(line);
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            System.out.println("Match found: " + line);
+    public static void checkNames(HashSet<String> inputNameSet, HashSet<String> listNameSet) {
+        inputNameSet.stream().map(s -> s.toUpperCase()).collect(Collectors.toList());
+        listNameSet.stream().map(s -> s.toUpperCase()).collect(Collectors.toList());
+
+        System.out.println(inputNameSet);
+        if (inputNameSet.equals(listNameSet)) {
+            System.out.println("Match found:");
+            System.out.println(inputNameSet);
+            System.out.println(listNameSet);
         }
     }
 }
