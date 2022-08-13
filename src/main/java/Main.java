1aef859e68b0577c56f04f5aa277ff45cf8804a7
Martin A <martin.apalkov@gmail.com>
2022-08-13 12:51:30 +0300

    sortedset

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,7 @@ package src.main.java;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,13 +11,17 @@ public class Main {
         String[] inputNameArray;
         inputNameArray = args[0].split(" ");
 
-        HashSet<String> inputNameSet = new HashSet<String>(Arrays.asList(inputNameArray));
+        SortedSet<String> inputNameSet =
+                new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+        Collections.addAll(inputNameSet, inputNameArray);
 
         try (Scanner scanner = new Scanner(new File(args[1]))) {
             while (scanner.hasNext()) {
                 String[] listNameArray;
                 listNameArray = scanner.nextLine().split(" ");
-                HashSet<String> listNameSet = new HashSet<String>(Arrays.asList(listNameArray));
+                SortedSet<String> listNameSet =
+                        new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+                Collections.addAll(listNameSet, listNameArray);
                 checkNames(inputNameSet, listNameSet);
             }
         } catch (FileNotFoundException e) {
@@ -28,11 +29,7 @@ public class Main {
         }
     }
 
-    public static void checkNames(HashSet<String> inputNameSet, HashSet<String> listNameSet) {
-        inputNameSet.stream().map(s -> s.toUpperCase()).collect(Collectors.toList());
-        listNameSet.stream().map(s -> s.toUpperCase()).collect(Collectors.toList());
-
-        System.out.println(inputNameSet);
+    public static void checkNames(SortedSet<String> inputNameSet, SortedSet<String> listNameSet) {
         if (inputNameSet.equals(listNameSet)) {
             System.out.println("Match found:");
             System.out.println(inputNameSet);
